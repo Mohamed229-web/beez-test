a41103f1e9889408c8173163fab8daedd713d3da
Mohamed Ibrahim DJIBRILA <djibrilaibrahim14@gmail.com>
2025-09-19 06:29:59 +0200

    feat:Setting Stacks animation scroll

--- app/components/commons/elements/StackCard.tsx
+++ app/components/commons/elements/StackCard.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const StackCard: React.FC<{ stack?: React.ReactElement }> = ({ stack }) => {
   return (
-    <div className='w-[84px] h-[84px] rounded-xl p-1.5 bg-[#fdf6ee]'>
-      {stack && <span className='stackIcon'>{stack}</span>}
+    <div className='w-[84px] h-[84px] rounded-xl p-1.5 bg-[#fffaf58c]'>
+      {stack && <span className='stack-icon'>{stack}</span>}
     </div>
   );
 };
--- app/components/motions/StacksMotion.tsx
+++ app/components/motions/StacksMotion.tsx
@@ -1,12 +1,57 @@
 import React from 'react';
 import StackCard from '../commons/elements/StackCard';
-import { BiLogoTypescript } from 'react-icons/bi';
+import { BiLogoReact, BiLogoTailwindCss, BiLogoTypescript } from 'react-icons/bi';
+import { FaFigma } from 'react-icons/fa';
+import { SiShadcnui } from 'react-icons/si';
 
 const StacksMotion: React.FC = () => {
   return (
-    <div>
-      <StackCard />
-      <StackCard stack={<BiLogoTypescript className='w-6 h-6' />} />
+    <div className='stacks-motion-grid'>
+      <div className='stack-col-list marquee-track marquee-track--bottom'>
+        <StackCard />
+        <StackCard />
+        <StackCard />
+        {/* repeat for animation */}
+        <StackCard />
+        <StackCard />
+        <StackCard />
+      </div>
+      <div className='stack-col-list marquee-track marquee-track--top'>
+        <StackCard />
+        <StackCard stack={<BiLogoTailwindCss className='w-6 h-6' />} />
+        <StackCard />
+        {/* repeat for animation */}
+        <StackCard />
+        <StackCard stack={<BiLogoTailwindCss className='w-6 h-6' />} />
+        <StackCard />
+      </div>
+      <div className='stack-col-list marquee-track marquee-track--bottom'>
+        <StackCard stack={<FaFigma className='w-6 h-6' />} />
+        <StackCard stack={<SiShadcnui className='w-6 h-6' />} />
+        <StackCard />
+        {/* repeat for animation */}
+        <StackCard stack={<FaFigma className='w-6 h-6' />} />
+        <StackCard stack={<SiShadcnui className='w-6 h-6' />} />
+        <StackCard />
+      </div>
+      <div className='stack-col-list marquee-track marquee-track--top'>
+        <StackCard />
+        <StackCard stack={<BiLogoReact className='w-6 h-6' />} />
+        <StackCard />
+        {/* repeat for animation */}
+        <StackCard />
+        <StackCard stack={<BiLogoReact className='w-6 h-6' />} />
+        <StackCard />
+      </div>
+      <div className='stack-col-list marquee-track marquee-track--bottom'>
+        <StackCard stack={<BiLogoTypescript className='w-6 h-6' />} />
+        <StackCard />
+        <StackCard />
+        {/* repeat for animation */}
+        <StackCard stack={<BiLogoTypescript className='w-6 h-6' />} />
+        <StackCard />
+        <StackCard />
+      </div>
     </div>
   );
 };
